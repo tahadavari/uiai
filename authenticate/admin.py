337c7f001601186fee24a318476ad193d57ec4c2
Taha <tahadavari1381@gmail.com>
2022-04-22 23:44:10 +0430

    auth admin

--- authenticate/admin.py
+++ authenticate/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from uiai.authenticate.models import ForgetPasswordEmailToken
 # Register your models here.
+@admin.register(ForgetPasswordEmailToken)
+class SettingAdmin(admin.ModelAdmin):
+    exclude = ['hash']
